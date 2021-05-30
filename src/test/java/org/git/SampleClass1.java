4a7b14c362bf1b4c90e777c9be03bff274659908
vijay27anandan <vijay27anandan@gmail.com>
2021-05-30 09:52:07 +0530

    Vijay git sample

---
+++ src/test/java/org/git/SampleClass1.java
@@ -0,0 +1,26 @@
+package org.git;
+
+public class SampleClass1 {
+	
+	private void add() {
+		System.out.println("this method is add");
+	}
+	
+	private void sub() {
+		System.out.println("this method is sub");
+	}
+	
+	private void multiply() {
+		System.out.println("this method is multiply");
+	}
+	private void division() {
+		System.out.println("this method is division");
+	}
+public static void main(String[] args) {
+	SampleClass1 s = new SampleClass1();
+	s.add();
+	s.sub();
+	s.multiply();
+	s.division();
+}
+}
